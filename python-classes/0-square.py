f0ddf1578d6f39d1cd316971ea25835f03aa7578
Rinat <mahmudovrinat88@gmail.com>
2025-06-28 10:22:09 +0000

    buydbeybdh:

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-"This module defines a class Square with a private size attribute."
+"""This module defines a class Square that represents a square."""
+
+
 class Square:
+    """This class defines a square with a private size attribute."""
+
     def __init__(self, size):
+        """Initialize the square with a given size.
+
+        Args:
+            size (any): The size of the square (no type or value check).
+        """
         self.__size = size
